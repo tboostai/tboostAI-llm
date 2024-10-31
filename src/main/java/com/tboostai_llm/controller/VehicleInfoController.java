9964e736febe69a063112e9afdc561c80d35a0db
kingsley-tboostai <kingsley.li@tboostai.com>
2024-10-31 01:57:06 -0700

    Setup prod properties

--- src/main/java/com/tboostai_llm/controller/VehicleInfoController.java
+++ src/main/java/com/tboostai_llm/controller/VehicleInfoController.java
@@ -4,6 +4,8 @@ import com.tboostai_llm.entity.request.OpenAIRequest;
 import com.tboostai_llm.entity.response.FormattedDescription;
 import com.tboostai_llm.entity.response.SearchParamsResponse;
 import com.tboostai_llm.service.OpenAIService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import reactor.core.publisher.Mono;
 @RestController
 public class VehicleInfoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(VehicleInfoController.class);
     private final OpenAIService openAIService;
 
     public VehicleInfoController(OpenAIService openAIService) {
@@ -30,6 +33,7 @@ public class VehicleInfoController {
     @PostMapping("/description")
     public Mono<FormattedDescription> getDescriptionData(@RequestBody Object description) {
         try {
+            logger.info("Request Body is {}", description);
             return openAIService.beautifulDescriptions(description);
         } catch (Exception e) {
             throw new RuntimeException(e);
